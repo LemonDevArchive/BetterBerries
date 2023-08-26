b1e41f63811c95194b0a3971e350877790f14793
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-08-25 23:31:36 -0400

    1.0.6 - Added missing compostables

--- src/main/java/lemon_juice/better_berries/util/ModCompostables.java
+++ src/main/java/lemon_juice/better_berries/util/ModCompostables.java
@@ -18,12 +18,17 @@ public class ModCompostables {
         add(0.35F, ModItems.BLUEBERRY_JUICE.get());
         add(0.35F, ModItems.MALOBERRY_JUICE.get());
         add(0.35F, ModItems.RASPBERRY_JUICE.get());
+        add(0.35F, ModItems.SWEET_BERRY_JUICE.get());
+        add(0.35F, ModItems.GLOW_BERRY_JUICE.get());
 
         add(0.4F, ModItems.BLACKBERRY_SMOOTHIE.get());
         add(0.4F, ModItems.BLUEBERRY_SMOOTHIE.get());
         add(0.4F, ModItems.MALOBERRY_SMOOTHIE.get());
         add(0.4F, ModItems.RASPBERRY_SMOOTHIE.get());
+        add(0.4F, ModItems.SWEET_BERRY_SMOOTHIE.get());
+        add(0.4F, ModItems.GLOW_BERRY_SMOOTHIE.get());
 
+        add(0.9F, ModItems.BERRY_MEDLEY.get());
     }
 
     public static void add(float quantity, ItemLike itemLike) {
